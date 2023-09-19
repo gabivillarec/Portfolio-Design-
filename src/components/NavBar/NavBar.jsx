5b5939731995201f7e9c04cca433ca8d957f050e
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-09-19 18:21:07 -0300

    navBar , falta tipografia

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,28 @@
-
+import style from "./navBar.module.css"
+import { useNavigate , useLocation } from 'react-router-dom'
+import { useState } from 'react';
 
 const NavBar = ()=>{
 
+
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+
     return(
-        <>
-        <h1>hola</h1>
-        </>
+        <div className={style.container}>
+            <div className={style.contenedorLogo}>
+            <button onClick={()=> navigate('/Home')} className={style.logo}>AB</button>
+            <h3 onClick={()=> navigate('/Home')} className={style.name}>Antonio Beorchia</h3>
+            </div>
+
+            <div>
+            <button onClick={()=> navigate('/Works')} className={style.btn}><h3>Works</h3></button>
+            <button onClick={()=> navigate('/About')} className={style.btn}><h3>About</h3></button>
+            </div>   
+
+        </div>
     )
 };
 
