d7ba9ff40291995f743fbf1593cc75ec3ff4f318
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-09-26 18:31:30 -0300

    creado la section para cada proyecto, manejado con props

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,21 @@
-import style from "./home.module.css"
+import style from "./home.module.css";
 import NavBar from "../NavBar/NavBar";
-import Hero from "../Hero/Hero"
+import Hero from "../Hero/Hero";
+import Section from "../Section/Section";
 
 const Home = ()=>{
+
+    
 
     return(
         <div className={style.container}>
             <NavBar/>
             <Hero/>
+            <Section 
+            key = "0" 
+            titulo = "Proyecto 0"
+            text = "texto descriptivo que explica el proyecto , klhgalskgh jsafk jsfiw sjeiql ppremvcwo sialfjwq oiefowimcoq ioffqinoais qwgionso wqifgn." 
+            image = "https://davidhellmann.com/uploads/imager/uploads/work/concept-schwarzkopf-227/344/schwarzkopf_01_image_01_86ad6ebaa7a034c0be58ed4e71db04e2.jpg"/>
         </div>
     );
 }
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -7,6 +7,11 @@ const Section = (props) =>{
     return (
         <div className={style.container}>
 
+            <h2>{props.titulo}</h2>
+            <div className={style.line}>Fecha Proyecto tecnología</div>
+            <p>{props.text}</p>
+            <img className={style.img} src={props.image} alt=''/>
+            
         </div>
     )
 }
