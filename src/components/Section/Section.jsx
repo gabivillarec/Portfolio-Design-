6d8ac6ae789f9d156e8a0184a786789e0ca50b87
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-09-26 18:12:46 -0300

    sections

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,14 @@
+import style from "./section.module.css"
+
+
+
+const Section = (props) =>{
+
+    return (
+        <div className={style.container}>
+
+        </div>
+    )
+}
+
+ export default Section
